a1ee281d55b75e15cb6b61165d270ccf20fba6a5
maddev75 <msacko883@gmail.com>
2020-08-31 13:09:28 +0200

    Create fight2.js

---
+++ js/fight2.js
@@ -0,0 +1,26 @@
+class Soldat {
+    constructor(pseudo, sante, attaque, defendre, niveau) {
+        this.pseudo = pseudo;
+        this.classe = classe;
+        this.sante = sante;
+        this.attaque = attaque;
+        this.defendre = defendre;
+        this.niveau = 100;
+    }
+    
+    get informations() {
+        return this.pseudo + " (" + this.classe + ") a " + this.sante + " points de vie et est au niveau " + this.niveau + ".";
+      }
+    
+    verifierSante() {
+        if(this.sante <= 0) {
+            this.sante == 0;
+            console.log(this.pseudo + 'a perdu !');
+        }
+    }
+    attaquer(personnage) {
+        personnage.sante -= this.attaque;
+        console.log(this.pseudo + 'attaque ' + personnage.pseudo + 'en lançant sa lance (' + this.attaque + ' dégats)' );
+        personnage.verifierSante();
+    }
+}
