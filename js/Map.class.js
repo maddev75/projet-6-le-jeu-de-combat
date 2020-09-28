6353a62774fe1be37ed2b4c94ab3c491b34c54f0
maddev75 <msacko883@gmail.com>
2020-09-28 20:37:00 +0200

    POSITION TOUR

--- js/Map.class.js
+++ js/Map.class.js
@@ -1,12 +1,11 @@
 let weapons = ["gun", "mitraillette", "missile", "lance-rocket"];
-let players  = ['soldat1', 'soldat2'];
+let players = ['soldat1', 'soldat2'];
 
 class Map {
     constructor(container, columns, lines, walls, weapons, players) {
         this.col = columns;
         this.lgn = lines;
         this.mur = walls;
-        //this.yellow = yellow;
         this.container = container;
         this.weapons = weapons;
         this.players = players;
@@ -19,39 +18,38 @@ class Map {
         this.createElement('wall', this.mur, 0);
         this.createElement('weapon', this.weapons.length, 0);
         this.createElement('player', this.players.length, 3);
-        //this.createElement('yellow', this.yellow, 0);
-
+       /* this.setMooveValable('right', this.player1, 3);
+        this.setMooveValable('left', this.player1, 3);
+        this.setMooveValable('top', this.player1, 3);
+        this.setMooveValable('bottom', this.player1, 3);*/
     }
     createGrid() {
         const $jeu = $(this.container);
-        for(let lgn=0; lgn < this.lgn; lgn++){
+        for (let lgn = 0; lgn < this.lgn; lgn++) {
             const $lgn = $('<div>').addClass('row');
-            for(let col=0; col < this.col; col++){
+            for (let col = 0; col < this.col; col++) {
                 // on attribue un id pour récupérer plus tard la case
-                const $col = $('<div>').addClass('col').attr('id', 'col_'+ lgn + '_' + col);
+                const $col = $('<div>').addClass('col').attr('id', 'col_' + lgn + '_' + col);
                 $lgn.append($col);
                 $jeu.append($lgn);
-                    }
-                }
             }
-    createElement = (type, quantity, minimumDistance)=> {
+        }
+    }
+    createElement = (type, quantity, minimumDistance) => {
         let create;
-        for (let i =0; i < quantity; i++){
+        for (let i = 0; i < quantity; i++) {
             switch (type) {
                 case 'wall':
                     create = this.createWalls();
-                        break;
+                    break;
                 case 'weapon':
                     create = this.createWeapons(this.weapons, i);
-                        break;
+                    break;
                 case 'player':
                     create = this.createPlayers(this.players, i, minimumDistance);
-                        break;
-                /*case 'yellow':
-                    create = this.createYellow();
-                        break;*/
-                }
-            if(create === 0) {
+                    break;
+            }
+            if (create === 0) {
                 i--
             }
         }
@@ -60,118 +58,183 @@ class Map {
         let x = this.getRandomInt(this.col);
         let y = this.getRandomInt(this.lgn);
         // on récupère la case sélectionnée au hasard
-        let target = $('#col_'+ x + "_" + y);
+        let target = $('#col_' + x + "_" + y);
         // on récupère ses classes ds un tableau
-        console.log('target'+target[0].className);
+        console.log('target' + target[0].className);
         let classes = target[0].className.split(/\s+/);
-        console.log('classes'+classes);
+        console.log('classes' + classes);
         // on vérifie que la classe a pas déjà un wall
-        if(!classes.includes('wall')){
+        if (!classes.includes('wall')) {
             //si elle a pas de wall
             target.addClass('wall');
             console.log(target);
             return 1
-        }else{
+        } else {
             console.error("La case a déjà un mur");
             console.log(target);
             return 0
         }
-     }
+    }
     getRandomInt = (max) => {
         return Math.floor(Math.random() * Math.floor(max));
     }
     createWeapons = (weapons, index) => {
-        let y =  this.getRandomInt((this.lgn));
+        let y = this.getRandomInt((this.lgn));
         let x = this.getRandomInt((this.col));
-        let target = $('#col_'+ x + "_" + y);
+        let target = $('#col_' + x + "_" + y);
         console.log(target);
         //<div class="case vide"></div>
         console.log(target[0].className);
         //classes=["case","vide"];
         let classes = target[0].className.split(/\s+/);
         console.log(classes);
-       if(!classes.includes('weapon')&&!classes.includes('wall')&&!classes.includes('player')){
+        if (!classes.includes('weapon') && !classes.includes('wall') && !classes.includes('player')) {
             target.addClass('weapon');
             target.addClass(weapons[index]);
-        }else {
-              return 0
+        } else {
+            return 0
         }
     }
-    createPlayers = (players, index)=> {
+    createPlayers = (players, index) => {
         let y = this.getRandomInt((this.lgn));
         let x = this.getRandomInt((this.col));
-        let position = {x:x, y:y};
+        let position = { x: x, y: y };
         console.log(position);
-        if(index > 0){
-           position = this.keepSocialDistanceFrom(this.player1, 3);
-           }
-        let target = $('#col_'+ position.x + "_" + position.y);
+        if(index ===1) {
+        let tour = this.getRandomInt((this.players.length));
+        if (tour === 1) {
+            position = this.keepSocialDistanceFrom(this.player1, 3);
+            this.setMooveValableRight(this.player1, 3);
+        }
+        }else{
+            position = this.keepSocialDistanceFrom(this.player2, 3);
+            this.setMooveValableRight(this.player2, 3);
+        }
+        let target = $('#col_' + position.x + "_" + position.y);
         console.log(target);
         let classes = target[0].className.split(/\s+/);
-        if(!classes.includes('player')&&!classes.includes('wall')&&!classes.includes('weapon')){
+        if (!classes.includes('player') && !classes.includes('wall') && !classes.includes('weapon')) {
             target.addClass('player');
-            //target.addClass('yellow');
             target.addClass(players[index].pseudo);
-            if(index === 0) {
-                this.player1 = {x: position.x, y: position.y};
-                //this.player1.style.left('orange');
-            }else{
-                this.player2 = {x: position.x, y: position.y};
-                //this.player2.addClass('yelllow');
+            if (tour === 0) {
+                this.player1 = { x: position.x, y: position.y };
+                this.setMooveValableRight(this.player1, 3)
+            } else {
+                this.player2 = { x: position.x, y: position.y };
+                this.setMooveValableRight(this.player2, 3)
             }
-            }else{
-                console.error('cette case contient une classe');
-                return 0
-            }
+        } else {
+            console.error('cette case contient une classe');
+            return 0
         }
-        /*createYellow = ()=> {
-            let y = this.getRandomInt((this.lgn));
-            let x = this.getRandomInt((this.col));
-            let target = $('#col_'+ x + "_" + y);
-            console.log(target);
-            let classes = target[0].className.split(/\s+/);
-            if(!classes.includes('player')&&!classes.includes('yellow')&&!classes.includes('wall')&&!classes.includes('weapon')){
-                target.addClass('yellow');
-                //target.addClass('yellow');
-            }
-        }*/
-        keepSocialDistanceFrom = (joueur, nbCases)=> {
-            let j2 = {x:5, y:5}
+    }
+    keepSocialDistanceFrom = (joueur, nbCases) => {
+        let j2 = { x: 5, y: 5 }
+        console.log(j2);
+        console.log("type of j2:", typeof (j2));
+        let good = false;
+        while (!good) {
+            j2.y = this.getRandomInt((this.lgn));
+            j2.x = this.getRandomInt((this.col));
             console.log(j2);
-            console.log("type of j2:",typeof(j2));
-            let good = false;
-            while(!good) {
-            j2.y =  this.getRandomInt((this.lgn));
-            j2.x =  this.getRandomInt((this.col));
-            console.log(j2);
-            if(Math.abs(j2.x - joueur.x) > nbCases && Math.abs(j2.y - joueur.y) > nbCases) {
+            if (Math.abs(j2.x - joueur.x) > nbCases && Math.abs(j2.y - joueur.y) > nbCases) {
                 good = true;
                 return j2;
             }
-        
         }
     }
-    zoneDeplacement = (joueur, nbCases)=> {
-        let j2 = {x:5, y:5}
-        
-        console.log(j2);
-        console.log("type of j2:",typeof(j2));
-        let good = false;
-        while(!good) {
-        j2.y =  this.getRandomInt((this.lgn));
-        j2.x =  this.getRandomInt((this.col));
-        console.log(j2);
-        if(Math.abs(j2.x - joueur.x) === 1 && Math.abs(j2.y - joueur.y) ===1) {
-            good = true;
-            j2.addClass('yellow');
-            return j2;
+    setMooveValableRight = (joueur, nbCases) => {
+        for (let i = 1; i <= nbCases; i++) {
+            let caseString = `#col_${joueur.x}_${joueur.y + i}`
+            let boardCase = $(caseString);
+            //this.moovePosition(boardCase);
+           console.log(boardCase);
+            if (boardCase[0] !== undefined) {
+                let classes = boardCase[0].className.split(/\s+/);
+                if (!classes.includes('wall')) {
+                    boardCase.addClass('yellow');
+                } else {
+                    return 0
+                }
+            }
         }
+    }
+    /* moovePosition = (boardCase)=> {
+       if (boardCase[0] !== undefined) {
+        let classes = boardCase[0].className.split(/\s+/);
+            if (!classes.includes('wall')) {
+                boardCase.addClass('yellow');
+            } else {
+                return 0
+            }
+        }*/
+    setMooveValableLeft = (joueur, nbCases) => {
+        for (let i = 1; i <= nbCases; i++) {
+            let caseString = `#col_${joueur.x}_${joueur.y - i}`
+            let boardCase = $(caseString);
+            console.log(boardCase);
+            if (boardCase[0] !== undefined) {
+                let classes = boardCase[0].className.split(/\s+/);
+                if (!classes.includes('wall')) {
+                    boardCase.addClass('yellow');
+                } else {
+                    return 0
+                }
+            }
+        }
+    }
+    setMooveValableTop = (joueur, nbCases) => {
+        for (let i = 1; i <= nbCases; i++) {
+            let caseString = `#col_${joueur.x + i}_${joueur.y}`
+            let boardCase = $(caseString);
+            console.log(boardCase);
+            if (boardCase[0] !== undefined) {
+                let classes = boardCase[0].className.split(/\s+/);
+                if (!classes.includes('wall')) {
+                    boardCase.addClass('yellow');
+                } else {
+                    return 0
+                }
+            }
+        }
+    }
+    setMooveValableBottom = (joueur, nbCases) => {
+        for (let i = 1; i <= nbCases; i++) {
+            let caseString = `#col_${joueur.x - i}_${joueur.y}`
+            let boardCase = $(caseString);
+            console.log(boardCase);
+            if (boardCase[0] !== undefined) {
+                let classes = boardCase[0].className.split(/\s+/);
+                if (!classes.includes('wall')) {
+                    boardCase.addClass('yellow');
+                } else {
+                    return 0
+                }
+            }
+        }
+    }
+    setMooveValable = (rotation, joueur, nbCases) => {
+        let moove;
+        for (let i = 1; i < nbCases; i++) {
+            switch (rotation) {
+                case 'right':
+                    moove = this.setMooveValableRight(joueur, nbCases);
+                    break;
+                case 'left':
+                    moove = this.setMooveValableLeft(joueur, nbCases);
+                    break;
+                case 'top':
+                    moove = this.setMooveValableTop(joueur, nbCases);
+                    break;
+                case 'bottom':
+                    moove = this.setMooveValableBottom(joueur, nbCases);
+                    break;
+            }
+        }
+    }
 }
-  /* zoneDeplacemnt = ()=> {
-        this.player1.addClass('yellow');
-        this.player2.addClass('yelllow');
-    }
-}*/
+
+
 
 
 
