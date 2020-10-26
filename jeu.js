4fb1856edef96a90c3a14a498a8cbd33d1b28919
maddev75 <msacko883@gmail.com>
2020-10-26 20:14:31 +0100

    modif mouvement
    modif mouvement

--- jeu.js
+++ jeu.js
@@ -3,7 +3,6 @@ $(function() {
 getRandomInt = (max) => { 
         return Math.floor(Math.random() * Math.floor(max));
     }
-    
 let weapons = ["gun", "mitraillette", "missile", "lance-rocket"];
 let player1 = new Soldat('soldat1', 100, 10, 0, 100, {x:0, y:0});
 let player2 = new Soldat('soldat2', 100, 10, 0, 100, {x:0, y:0});
@@ -22,33 +21,54 @@ let currentPlayerNb = getRandomInt(players.length);
 displayMoves(currentPlayer, map);
 listenMoves(map, currentPlayer);
 });
-
 function displayMoves(currentPlayer, map) {
     map.setMooveValable('right', currentPlayer, 3);
     map.setMooveValable('left', currentPlayer, 3);
     map.setMooveValable('top', currentPlayer, 3);
     map.setMooveValable('bottom', currentPlayer, 3);
 }
-function listenMoves(map, currentPlayer) {
+function listenMoves(map, currentPlayer){
+    let player 
     //let moveAvailableCases = document.getElementsByClassName('yellow');
     let moveAvailableCases = $('.yellow');
-    console.log(moveAvailableCases);
     Array.from(moveAvailableCases).forEach(moveCase => {
-       // moveCase.addEventListener('click', (event)=> {
-           $(moveCase).click(()=> {
-            let caseInfo = event.target.id.split('_')
-            $(currentPlayer).animate({right: '100px'})
-            console.log('case info'+caseInfo)
-          // })
-            currentPlayer.position = {x: caseInfo[1], y:0}
-            //console.log(player);
-        if(currentPlayer === map.players[0]){
-            player = map.players[1]
-        }else{
-            player = map.players[0]
-        }
-        console.log(player);
+       moveCase.addEventListener('click', (event)=> {
+           //$(moveCase).click(()=> {
+             caseInfo = event.target.id.split('_')
+             bouge();
+            //currentPlayer.animate({left:'100px'}, 1000);
+            console.log('case info'+ caseInfo)
+            //currentPlayer.position = {x: caseInfo[1], y:0}
+           
+            if(currentPlayer === map.players[0]){
+                player = map.players[1]
+                console.log(currentPlayer.position);
+            //$(currentPlayer).animate({left:'100px'}, 1000);
+
+            }else{
+                player = map.players[0]
+                console.log(currentPlayer.position);
+
+                //$(currentPlayer).animate({left:'100px'}, 1000);
+                }
         displayMoves(player, map);
         })
+    })
+}function bouge(){
+    let currentPlayer;
+    let currentPlayerNb = getRandomInt(players.length);
+    if (currentPlayerNb === 0) {
+        alert('Le soldat 1 commence')
+    currentPlayer = map.players[0]
+    }else{
+        alert('Le soldat 2 commence')
+        currentPlayer = map.players[1]
+    }
+    currentPlayer.style.top = 100;
+    requestAnimationFrame(bouge);
+}
+
+
+
 
 
